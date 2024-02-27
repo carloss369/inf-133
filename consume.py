f58b30ae985f37319bd7461a08b8d0a95eb7ea0c
carloss369 <117764213+carloss369@users.noreply.github.com>
2024-02-27 22:31:50 +0000

    comitt

---
+++ consume.py
@@ -0,0 +1,8 @@
+from zeep import Client
+
+client=Client(
+    "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL"
+
+)
+result = client.service.NumberToDollars(5)
+print(result)
